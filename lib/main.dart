3db7800783a1f661f1a48c3316276ef7bf6da098
Ruksar Ahmed <ruksar.org@gmail.com>
2021-04-14 22:17:00 +0530

    Basic Ecommerce Flutter App

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        backgroundColor: Colors.red,
+        title: Text("Sellerfort"),
+        actions: [
+          new IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+              onPressed: () {}),
+          new IconButton(
+              icon: Icon(
+                Icons.shopping_cart,
+                color: Colors.white,
+              ),
+              onPressed: () {}),
+        ],
+      ),
+      drawer: new Drawer(
+        child: new ListView(
+          children: [
+            // Header
+            new UserAccountsDrawerHeader(
+              accountName: Text("Ruksar Ahmed"),
+              accountEmail: Text("ruksar@sellerfort.com"),
+              currentAccountPicture: GestureDetector(
+                child: new CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  child: Icon(
+                    Icons.person,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              decoration: new BoxDecoration(color: Colors.red),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
